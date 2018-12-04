f84b88e53991f7d3f970df3cdec7311c5670b1d5
Deepesh Kumar Nath <deepesh@ost.com>
2018-12-05 00:28:52 +0530

    Revert changes

--- truffle.js
+++ truffle.js
@@ -1,19 +1,10 @@
 module.exports = {
   networks: {
-    ganache: {
+    development: {
       host: "127.0.0.1",
       network_id: "*", // Match any network id
       port: 8545,
-      gas: 12000000,
-      gasPrice: 0x01,
-    },
-    coverage: {
-      host: "127.0.0.1",
-      network_id: "*",
-      port: 8555,         // <-- If you change this, also set the port option in .solcover.js.
-      gas: 0xfffffffffff, // <-- Use this high gas value 
-      gasPrice: 0x01,     // <-- Use this low gas price
-    },
+    }
   },
   compilers: {
     solc: {
