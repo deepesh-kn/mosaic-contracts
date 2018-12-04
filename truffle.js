c03fb8ff26bfd275c4bf288f0b90bcb2a52e358a
Deepesh Kumar Nath <deepesh@ost.com>
2018-12-05 00:14:05 +0530

    Changed localhost to 127.0.0.1

--- truffle.js
+++ truffle.js
@@ -1,14 +1,14 @@
 module.exports = {
   networks: {
     development: {
-      host: "localhost",
+      host: "127.0.0.1",
       network_id: "*", // Match any network id
       port: 8545,
       gas: 12000000,
       gasPrice: 0x01,
     },
     coverage: {
-      host: "localhost",
+      host: "127.0.0.1",
       network_id: "*",
       port: 8555,         // <-- If you change this, also set the port option in .solcover.js.
       gas: 0xfffffffffff, // <-- Use this high gas value 
