98eeb6e955f6ec3dd93b9e8b4a6d976397a597d0
Deepesh K N <deepesh@ost.com>
2019-07-14 20:02:23 +0530

    Updated gitignore

--- dist/index.js
+++ dist/index.js
@@ -1,23 +1,19 @@
-// Copyright 2019 OpenST Ltd.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//    http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-//
-// ----------------------------------------------------------------------------
-//
-// http://www.simpletoken.org/
-//
-// ----------------------------------------------------------------------------
-
-const contracts = require('./contracts.json');
-
-module.exports = contracts;
+"use strict";
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
+    result["default"] = mod;
+    return result;
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const contracts = __importStar(require("./contract_build/contracts.json"));
+exports.contracts = contracts;
+const Interacts_1 = __importDefault(require("./interacts/Interacts"));
+exports.interacts = Interacts_1.default;
+const deployers = __importStar(require("./deployers/Deployer"));
+exports.deployers = deployers;
+//# sourceMappingURL=index.js.map
